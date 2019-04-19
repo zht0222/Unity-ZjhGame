078409d0161b328265816a783c3a77afe8d4faa3
zhnaghaitao <563429136@qq.com>
2019-04-19 11:18:15 +0800

    炸金花客户端和服务端

---
+++ MyServer/GameServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyServer;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerPeer server = new ServerPeer();
+            server.SetApplication(new NetMsgCenter());
+            server.StartServer("172.26.228.2", 6666, 100);
+            Database.DatabaseManager.StartConnect();
+            Console.ReadKey();
+        }
+    }
+}
